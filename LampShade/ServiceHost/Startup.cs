d994bd8e1a4ec5f856d3eb7c3d57c51bc66c9fb9
Javadi520 <Javadi5520@Gmail.com>
2021-07-17 00:42:41 +0430

    Comment Management System Implemented

--- LampShade/ServiceHost/Startup.cs
+++ LampShade/ServiceHost/Startup.cs
@@ -1,3 +1,5 @@
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using _0_Framework.Application;
 using BlogManagement.Infrastructure.Configuration;
 using CommentManagement.Infrastructure.Configuration;
@@ -30,6 +32,8 @@ namespace ServiceHost
             InventoryManagementBootstrapper.Configure(services, connectionString);
             CommentManagementBootstrapper.Configure(services, connectionString);
             BlogManagementBootstrapper.Configure(services, connectionString);
+
+            services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Arabic));
 
             services.AddTransient<IFileUploader, FileUploader>();
             services.AddRazorPages();
