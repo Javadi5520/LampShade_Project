71c0aea558dadfd8dc3d67170ea2ad5bc7f5faf9
Javadi520 <Javadi5520@Gmail.com>
2021-07-18 03:18:14 +0430

    Authentication Implemented

--- LampShade/ServiceHost/Startup.cs
+++ LampShade/ServiceHost/Startup.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 using _0_Framework.Application;
+using _0_Framework.Infrastructure;
+using AccountManagement.Configuration;
 using BlogManagement.Infrastructure.Configuration;
 using CommentManagement.Infrastructure.Configuration;
 using DiscountManagement.Configuration;
 using InventoryManagement.Infrastructure.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,16 +31,51 @@ namespace ServiceHost
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
             var connectionString = Configuration.GetConnectionString("LampshadeDb");
             ShopManagementBootstrapper.Configure(services, connectionString);
             DiscountManagementBootstrapper.Configure(services, connectionString);
             InventoryManagementBootstrapper.Configure(services, connectionString);
-            CommentManagementBootstrapper.Configure(services, connectionString);
             BlogManagementBootstrapper.Configure(services, connectionString);
+            CommentManagementBootstrapper.Configure(services, connectionString);
+            AccountManagementBootstrapper.Configure(services, connectionString);
 
             services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Arabic));
-
+            services.AddSingleton<IPasswordHasher, PasswordHasher>();
             services.AddTransient<IFileUploader, FileUploader>();
+            services.AddTransient<IAuthHelper, AuthHelper>();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.Lax;
+            }); 
+            
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, o =>
+                {
+                    o.LoginPath = new PathString("/Account");
+                    o.LogoutPath = new PathString("/Account");
+                    //o.AccessDeniedPath = new PathString("/AccessDenied");
+                });
+
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("AdminArea",
+            //        builder => builder.RequireRole(new List<string> { Roles.Administrator, Roles.ContentUploader }));
+
+            //    options.AddPolicy("Shop",
+            //        builder => builder.RequireRole(new List<string> { Roles.Administrator }));
+
+            //    options.AddPolicy("Discount",
+            //        builder => builder.RequireRole(new List<string> { Roles.Administrator }));
+
+            //    options.AddPolicy("Account",
+            //        builder => builder.RequireRole(new List<string> { Roles.Administrator }));
+            //});
+
+
+
             services.AddRazorPages();
         }
 
@@ -51,16 +91,24 @@ namespace ServiceHost
                 app.UseExceptionHandler("/Error");
             }
 
+            app.UseAuthentication();
+
+            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+            //app.UseCors("MyPolicy");
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
-                endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllers();
             });
         }
     }
